52b8f2ba06af282085296677fc076f521e042c57
Sebastian Gustafsson <sebastian.gustafsson@utb.ecutbildning.se>
2021-09-14 15:40:25 +0200

    [Red] - TestAllOnes blir fel

--- TestProject1/BowlingGameTests.cs
+++ TestProject1/BowlingGameTests.cs
@@ -23,5 +23,16 @@ namespace TestProject1
 
             Assert.Equal(0, game.Score());
         }
+
+        [Fact]
+        public void TestAllOnes()
+        {
+            Game game = new Game();
+
+            for (int i = 0; i < 20; i++)
+                game.Roll(1);
+
+            Assert.Equal(20, game.Score());
+        }
     }
 }
